d0dcb58b04e91a746242b7464b9af6a01876274f
Carmelo Velardo <carmelo.velardo@eng.ox.ac.uk>
2016-01-25 21:26:11 +0000

    Added dependency from openpyxl and converted the workflow so that it reads data from XLSX files

--- converter.py
+++ converter.py
@@ -16,7 +16,7 @@ import re
 import fnmatch
 import csv
 import json
-
+from openpyxl import load_workbook
 
 def convertOCType(text):
     if text == 'radio': return 'radiobuttons'
@@ -39,6 +39,19 @@ def combineOptions(opt, vals):
     return OptionsElement
 
 
+row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA']
+
+def get(sheet, c, r):
+    return sheet[row[c] + str(r)]
+def getVal(sheet, c, r):
+    if get(sheet, c, r) != None:
+        return get(sheet, c, r).value
+    else :
+        return None
+def isOK(data):
+    return data != None and data != ''
+
+
 
 if __name__ == '__main__':
 
@@ -49,7 +62,7 @@ if __name__ == '__main__':
     usage = "usage: %prog [options] arg"
     parser = OptionParser(usage)
 
-    parser.add_option("-i", "--input", dest="input", help="input CRF file")
+    parser.add_option("-i", "--input", dest="input", help="input CRF XLSX file")
     parser.add_option("-o", "--output", dest="output", help="output elaborated html")
     parser.add_option("-u", "--user_db", help="user of database. ")
     parser.add_option("-p", "--pass_db", help="password of database. ")
@@ -84,65 +97,82 @@ if __name__ == '__main__':
     if input is None:
         parser.error("You must provide at least a valid input files.")
 
+    # Load the Workbook
+    workbook = load_workbook(options.input)
+    sheet_groups = workbook['Groups']
+    sheet_sections = workbook['Sections']
+    sheet_items = workbook['Items']
 
-    reader = csv.reader(input, delimiter=',')
+    starting_ind = 2
+    Dictionary_Groups = {}
 
-    lineNumber=0
+    while isOK(getVal(sheet_groups, 0, starting_ind)) :
+        Dictionary_Groups[getVal(sheet_groups, 0, starting_ind)] = getVal(sheet_groups, 2, starting_ind)
+        print('Added ' + getVal(sheet_groups, 0, starting_ind) + ' ' + Dictionary_Groups[getVal(sheet_groups, 0, starting_ind)])
+        starting_ind += 1
+
 
     '''
-    This is the schema
-    0	name	            ITEM_NAME
-    1	label_short	        DESCRIPTION_LABEL
-    2	caption	            LEFT_ITEM_TEXT
-    3	units	            UNITS
-    4		                RIGHT_ITEM_TEXT
-    5	section	            SECTION_LABEL
-    6	group	            GROUP_LABEL
-    7		                HEADER
-    8		                SUBHEADER
-    9		                PARENT_ITEM
-    10		                COLUMN_NUMBER
-    11		                PAGE_NUMBER
-    12		                QUESTION_NUMBER
-    13	type	            RESPONSE_TYPE
-    14	???	                RESPONSE_LABEL
-    15	options	            RESPONSE_OPTIONS_TEXT
-    16	options_values	    RESPONSE_VALUES_OR_CALCULATIONS
-    17		                RESPONSE_LAYOUT
-    18		                DEFAULT_VALUE
-    19	data_type	        DATA_TYPE
-    20		                WIDTH_DECIMAL
-    21	validation_regex	VALIDATION
-    22	validation_message	VALIDATION_ERROR_MESSAGE
-    23		                PHI
-    24	required	        REQUIRED
-    25	display_status  	ITEM_DISPLAY_STATUS
-    26	display_condition	SIMPLE_CONDITIONAL_DISPLAY
+    This is the schema for the Items sheet
+    A 0	name	            ITEM_NAME
+    B 1	label_short	        DESCRIPTION_LABEL
+    C 2	caption	            LEFT_ITEM_TEXT
+    D 3	units	            UNITS
+    E 4		                RIGHT_ITEM_TEXT
+    F 5	section	            SECTION_LABEL
+    G 6	group	            GROUP_LABEL
+    H 7		                HEADER
+    I 8		                SUBHEADER
+    J 9		                PARENT_ITEM
+    K 10		                COLUMN_NUMBER
+    L 11		                PAGE_NUMBER
+    M 12		                QUESTION_NUMBER
+    N 13	type	            RESPONSE_TYPE
+    O 14	???	                RESPONSE_LABEL
+    P 15	options	            RESPONSE_OPTIONS_TEXT
+    Q 16	options_values	    RESPONSE_VALUES_OR_CALCULATIONS
+    R 17		                RESPONSE_LAYOUT
+    S 18		                DEFAULT_VALUE
+    T 19	data_type	        DATA_TYPE
+    U 20		                WIDTH_DECIMAL
+    V 21	validation_regex	VALIDATION
+    W 22	validation_message	VALIDATION_ERROR_MESSAGE
+    X 23		                PHI
+    Y 24	required	        REQUIRED
+    Z 25	display_status  	ITEM_DISPLAY_STATUS
+    AA 26	display_condition	SIMPLE_CONDITIONAL_DISPLAY
     '''
-
-    def isOK(data):
-        return data != None and data != ''
-
-
     clusterSection = ''
     clusterGroup = ''
 
     ALL_FORM_ELEMENTS = {}
     DICTIONARY_ELEMENTS = {}
 
+    lineNumber=0
+    ind = 1
 
-    for row in reader:
+    SI = sheet_items
 
-        lineNumber+=1
-        if len(row) < 1 or lineNumber == 1:
-            continue
-        if row[0] == "" or row[0] is None or row[13] == "" or row[13] is None:
+    while isOK(getVal(SI,0,ind)):
+
+        ind += 1
+
+        print('Elaborating row ' + str(ind))
+        #    for row in reader:
+        # lineNumber+=1
+        # if len(row) < 1 or lineNumber == 1:
+        #     continue
+        # if row[0] == "" or row[0] is None or row[13] == "" or row[13] is None:
+        #     continue
+
+        if not isOK(getVal(SI,0,ind)) or not isOK(getVal(SI,13,ind)):
             continue
 
         section = ''
         sectionFull = ''
-        if isOK(row[5]):
-            sectionFull = row[5]
+
+        if isOK(getVal(SI,5,ind)):
+            sectionFull = getVal(SI,5,ind)
         section = "".join(sectionFull.split())
 
         if section != '' and section is not None:
@@ -152,28 +182,28 @@ if __name__ == '__main__':
                 DICTIONARY_ELEMENTS[section] = []
 
 
-        if clusterSection != row[5]:
+        if clusterSection != getVal(SI,5,ind):
             SINGLE_ELEMENT = {}
             SINGLE_ELEMENT['type'] = 'p'
             SINGLE_ELEMENT['class'] = 'section'
-            SINGLE_ELEMENT['html'] = row[5]
+            SINGLE_ELEMENT['html'] = getVal(SI,5,ind)
             ALL_FORM_ELEMENTS[section].append(SINGLE_ELEMENT)
-            clusterSection = row[5]
+            clusterSection = getVal(SI,5,ind)
 
-        if clusterGroup != row[6]:
+        if clusterGroup != getVal(SI,6,ind):
             SINGLE_ELEMENT = {}
             SINGLE_ELEMENT['type'] = 'p'
             SINGLE_ELEMENT['class'] = 'group'
-            SINGLE_ELEMENT['html'] = row[6]
+            SINGLE_ELEMENT['html'] = Dictionary_Groups[getVal(SI,6,ind)]
             ALL_FORM_ELEMENTS[section].append(SINGLE_ELEMENT)
-            clusterGroup = row[6]
+            clusterGroup = getVal(SI,6,ind)
 
 
         SINGLE_ELEMENT = {}
 
-        SINGLE_ELEMENT['name'] = row[0]
-        SINGLE_ELEMENT['id'] = row[0]
-        SINGLE_ELEMENT['type'] = convertOCType(row[13].strip())
+        SINGLE_ELEMENT['name'] = getVal(SI,0,ind)
+        SINGLE_ELEMENT['id'] = getVal(SI,0,ind)
+        SINGLE_ELEMENT['type'] = convertOCType(getVal(SI,13,ind).strip())
 
         # In case of checkboxes transform the element
         # into an array otherwise we will receive only
@@ -184,32 +214,32 @@ if __name__ == '__main__':
 
         validate = {}
 
-        if isOK(row[24]) and (row[24] == '1' or row[24] == 1):
+        if isOK(getVal(SI,24,ind)) and (getVal(SI,24,ind) == '1' or getVal(SI,24,ind) == 1):
             SINGLE_ELEMENT['validate'] = {'required': True, 'min': 0}
 
 
-        if isOK(row[2]):
-            SINGLE_ELEMENT['caption'] = row[2]
+        if isOK(getVal(SI,2,ind)):
+            SINGLE_ELEMENT['caption'] = getVal(SI,2,ind)
 
-        if isOK(row[15]) and isOK(row[16]):
-            SINGLE_ELEMENT['options'] = combineOptions(row[15], row[16])
+        if isOK(getVal(SI,15,ind)) and isOK(getVal(SI,16,ind)):
+            SINGLE_ELEMENT['options'] = combineOptions(getVal(SI,15,ind), getVal(SI,16,ind))
 
         # Activates the datepicker
-        if isOK(row[19]) and row[19] == 'DATE':
+        if isOK(getVal(SI,19,ind)) and getVal(SI,19,ind) == 'DATE':
             SINGLE_ELEMENT['datepicker'] = {}
 
         # Substitute the type with number
-        if SINGLE_ELEMENT['type'] == 'text' and isOK(row[19]) and (row[19] == 'INT' or row[19] == 'REAL'):
+        if SINGLE_ELEMENT['type'] == 'text' and isOK(getVal(SI,19,ind)) and (getVal(SI,19,ind) == 'INT' or getVal(SI,19,ind) == 'REAL'):
             SINGLE_ELEMENT['type'] = 'number'
 
-        if isOK(row[3]):
-            SINGLE_ELEMENT['units'] = row[3]
+        if isOK(getVal(SI,3,ind)):
+            SINGLE_ELEMENT['units'] = getVal(SI,3,ind)
 
-        if isOK(row[5]):
-            SINGLE_ELEMENT['section'] = row[5]
+        if isOK(getVal(SI,5,ind)):
+            SINGLE_ELEMENT['section'] = getVal(SI,5,ind)
 
-        if isOK(row[6]):
-            SINGLE_ELEMENT['oc_group'] = row[6]
+        if isOK(getVal(SI,6,ind)):
+            SINGLE_ELEMENT['oc_group'] = getVal(SI,6,ind)
 
         ENCLOSING_ENVELOP = {}
         ENCLOSING_ENVELOP['type'] = 'div'
